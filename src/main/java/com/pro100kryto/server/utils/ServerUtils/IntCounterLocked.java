905385c96e4967eb1e5573067605aee9f583d588
pro100kryto <pro100kryto.original@gmail.com>
2021-05-01 22:21:58 -0300

    Refactoring IntCounterLocked

--- src/main/java/com/pro100kryto/server/utils/ServerUtils/IntCounterLocked.java
+++ src/main/java/com/pro100kryto/server/utils/ServerUtils/IntCounterLocked.java
@@ -1,10 +1,9 @@
 package com.pro100kryto.server.utils.ServerUtils;
 
 public class IntCounterLocked {
-    private int i;
-    private final int minValue;
-    private final int maxValue;
-    private final Object locker = new Object();
+    private volatile int i;
+    private int minValue;
+    private int maxValue;
 
     public IntCounterLocked() {
         this(0);
@@ -20,31 +19,53 @@ public class IntCounterLocked {
         maxValue = max;
     }
 
-    public int incrementAndGet() {
-        synchronized (locker) {
-            i++;
-            if (i > maxValue) i = minValue;
-            return i;
-        }
+    public synchronized int incrementAndGet() {
+        ++i;
+        if (i > maxValue) i = minValue;
+        return i;
     }
 
-    public int decrementAndGet() {
-        synchronized (locker) {
-            i--;
-            if (i < minValue) i = maxValue;
-            return i;
-        }
+    public synchronized int decrementAndGet() {
+        ++i;
+        if (i < minValue) i = maxValue;
+        return i;
     }
 
-    public void setValue(int value) {
-        synchronized (locker) {
-            i = value;
-        }
+    public synchronized int getAndIncrement() {
+        final int v = i;
+        ++i;
+        if (i > maxValue) i = minValue;
+        return v;
     }
 
-    public int get() {
-        synchronized (locker) {
-            return i;
-        }
+    public synchronized int getAndDecrement() {
+        final int v = i;
+        --i;
+        if (i < minValue) i = maxValue;
+        return v;
+    }
+
+    public synchronized void setValue(int value) {
+        i = value;
+    }
+
+    public synchronized int get() {
+        return i;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public synchronized void setMinValue(int minValue) {
+        this.minValue = minValue;
+    }
+
+    public synchronized void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
     }
 }
